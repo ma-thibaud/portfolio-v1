ed7dae5f19c6c1c0d9d30d00d838e440038643ee
Mathias Thibaud <math.thibaud@gmail.com>
2021-01-10 12:07:12 -0800

    Added Medicina project

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,23 +4,30 @@ Project.destroy_all
 
 puts "Creating projects..."
 
+medicina = Project.create!(
+  title: "Medicina France",
+  description: "Freelance project / Corporate website for French medical company / Features: product pages, search bar, contact form / Tech: Rails, React, Heroku",
+  website_url: "https://www.medicinafrance.fr/",
+  image_url: "https://res.cloudinary.com/darvd5cv4/image/upload/v1610308176/jpfoltkuvassmwxz1aq0.png"
+  )
+
 yup = Project.create!(
-  title: "Yup!",
-  description: "Your Urban Potager assists you in the preparation and planning of your own veggies, with to-do list and full-view calendar.",
+  title: "Your Urban Potager",
+  description: "School project / Gardening app / Features: to-do list, full-view calendar, payment / Tech: Rails, Stripe, Heroku",
   website_url: "http://www.yoururbanpotager.com/",
   image_url: "https://res.cloudinary.com/darvd5cv4/image/upload/v1582658424/miniature-yup_k18kvh.png"
   )
 
 rdvakorpo = Project.create!(
   title: "Rendez-vous à Korpo",
-  description: "Project created for an event in order to share useful information with attendees.",
+  description: "Freelance project / Event website for family reunion / Features: event information, interactive map / Tech: Rails, Mapbox, Heroku",
   website_url: "http://www.rdvakorpo.com/",
   image_url: "https://res.cloudinary.com/darvd5cv4/image/upload/v1582658424/Miniature-Korpo_sn79jx.png"
   )
 
 sample_app = Project.create!(
   title: "Twitter-style Microblog",
-  description: "Personal project to create a site with Twitter-like microposts and a status feed of posts from followed users.",
+  description: "School project / Twitter-like app / Features: authentication, microposts, status feed / Tech: Rails, Heroku",
   website_url: "https://obscure-reaches-21485.herokuapp.com/",
   image_url: "https://res.cloudinary.com/darvd5cv4/image/upload/v1582853545/Miniature-SampleApp-3_tabr0z.png"
   )
