b5a2015f4e455dfad0982f4b160e1ccd0778eb46
Mathias Thibaud <math.thibaud@gmail.com>
2021-01-10 17:08:39 -0800

    Minor CSS changes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,5 @@
 class Project < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
-  validates :website_url, presence: true
   validates :image_url, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,26 @@ medicina = Project.create!(
   title: "Medicina France",
   description: "Freelance project / Corporate website for French medical company / Features: product pages, search bar, contact form / Tech: Rails, React, Heroku",
   website_url: "https://www.medicinafrance.fr/",
-  image_url: "https://res.cloudinary.com/darvd5cv4/image/upload/v1610308176/jpfoltkuvassmwxz1aq0.png"
+  image_url: "https://res.cloudinary.com/darvd5cv4/image/upload/v1610324975/mrxclxhlzqjffi8rxfcy.png"
   )
 
 yup = Project.create!(
   title: "Your Urban Potager",
   description: "School project / Gardening app / Features: to-do list, full-view calendar, payment / Tech: Rails, Stripe, Heroku",
-  website_url: "http://www.yoururbanpotager.com/",
+  website_url: "http://yoururbanpotager.herokuapp.com/",
   image_url: "https://res.cloudinary.com/darvd5cv4/image/upload/v1582658424/miniature-yup_k18kvh.png"
   )
 
 rdvakorpo = Project.create!(
   title: "Rendez-vous à Korpo",
   description: "Freelance project / Event website for family reunion / Features: event information, interactive map / Tech: Rails, Mapbox, Heroku",
-  website_url: "http://www.rdvakorpo.com/",
+  website_url: "https://rdvakorpo.herokuapp.com/",
   image_url: "https://res.cloudinary.com/darvd5cv4/image/upload/v1582658424/Miniature-Korpo_sn79jx.png"
   )
 
 sample_app = Project.create!(
   title: "Twitter-style Microblog",
   description: "School project / Twitter-like app / Features: authentication, microposts, status feed / Tech: Rails, Heroku",
-  website_url: "https://obscure-reaches-21485.herokuapp.com/",
   image_url: "https://res.cloudinary.com/darvd5cv4/image/upload/v1582853545/Miniature-SampleApp-3_tabr0z.png"
   )
 
